e8c45ec0d821dfab929026973a66eee6ef328315
Marvellous Akib <marvellous.akib@gmail.com>
2024-03-12 17:37:54 +0000

    completed the missing test for the sum function

---
+++ calculator.test.js
@@ -0,0 +1,62 @@
+const {
+  sum,
+  subtract,
+  multiply,
+  divide,
+  modulus,
+  even,
+  odd,
+} = require("./calculator");
+
+describe('sum', () => { 
+  
+  test('can add two small positive numbers', () => {
+    expected = 5;
+    actual = sum(2, 3);
+    expect(actual).toBe(expected);
+  });
+
+  test('can add two large positive numbers', () => { 
+    expected = 101;
+    actual = sum(49,52);
+    expect(actual).toBe(expected);
+  });
+
+  test('can add two negative numbers', () => {
+    expected = -9;
+    actual = sum(-5,-4);
+    expect(actual).toBe(expected);
+  });
+
+  test('can add zero', () => {
+    expected = 4
+    actual = sum(4,0);
+    expect(actual).toBe(expected);
+  });
+
+});
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+describe('subtract', () => {
+
+});
+
+describe('multiply', () => {
+
+});
+
+describe('divide', () => {
+
+});
+
+describe('modulus', () => {
+
+});
+
+describe('even', () => {
+
+});
+
+describe('odd', () => {
+
+});
